022dc7f5f09bed236490f65585d866709782dd8c
SamandarDeveloper19 <mamadiyorovsamandar19@gmail.com>
2024-12-20 20:53:05 +0500

    Add ConvertToBinary Method

--- Program.cs
+++ Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ConvertToBinary();
+            Console.ReadKey();
+        }
+
+        static void ConvertToBinary()
+        {
+            Console.Write("10-lik sanoq sistemasidagi sonni kiriting: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number == 0)
+                Console.WriteLine("0");
+
+            string binary = "";
+
+            while (number > 0)
+            {
+                int remainder = number % 2;
+                binary = remainder + binary;
+                number /= 2;
+            }
+
+            Console.WriteLine($"2-lik sanoq sistemasida: {binary}");
         }
     }
 }
